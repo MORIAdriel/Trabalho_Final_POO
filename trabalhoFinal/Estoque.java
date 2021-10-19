94ecf74b1889936878dd5a3b0b72bed892e0288b
Adriel Lenner Vinhal Mori <82418789+MORIAdriel@users.noreply.github.com>
2021-10-18 21:32:21 -0300

    Update Estoque.java

--- trabalhoFinal/Estoque.java
+++ trabalhoFinal/Estoque.java
@@ -2,6 +2,41 @@ package trabalhoFinal;
 
 import java.util.ArrayList;
 
-public class Estoque {
- 
+public class Estoque implements Funcoes{
+
+	private int capacidadeMax;
+	ArrayList<Produtos> produtosEmEstoque = new ArrayList<>();
+	
+	Estoque(int capacidadeMax, ArrayList<Produtos> produtosEmEstoque) {
+		this.capacidadeMax = capacidadeMax;
+		this.produtosEmEstoque = produtosEmEstoque;
+	}
+	
+	public void listagemProdutoNoEstoque(Endereco procurado) {
+
+	}
+
+	@Override
+	public void consulta(String nome) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alteracaoCadastros() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void capacidadeEstoque() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean verificaEstoque() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
