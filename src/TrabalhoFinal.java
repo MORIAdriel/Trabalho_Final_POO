9f783c5f507aefd5d5f4a725a3645a4133fe5ca0
igorpadua <igorpadua@tutanota.com>
2021-10-26 16:13:30 -0300

    Ideia inicial do projeto

--- src/TrabalhoFinal.java
+++ src/TrabalhoFinal.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /*
  * Classe principal do programa  
@@ -9,6 +10,41 @@ import java.util.ArrayList;
 
 public class TrabalhoFinal {
 	public static void main(String[] args) {
+		Boolean sair = true;
+		String escolha;
 
+		while (sair == true) {
+			escolha = JOptionPane.showInputDialog(null, 
+					"1 - Adicionar uma empresa\n" + 
+					"2 - Adicionar um produto\n" +
+					"3 - Procurar um produto por endereço\n" +
+					"4 - Modificar algum dado\n" +
+					"5 - sair", 
+					"TrabalhoFinal", JOptionPane.QUESTION_MESSAGE);
+			switch (escolha) {
+				case "1":
+					/*
+					 * Aqui o usuário poderá criar uma loja ou um fornecedor
+					 */
+					break;
+				case "2":
+					/*
+					 * Aqui o usuário poderá adicionar um produto a alguma loja ou fornecedor
+					 */
+					break;
+				case "3":
+					/*
+					 * Aqui o usuário poderá pesquisar um endereço para encontrar lojas 
+					 */
+					break;
+				case "4":
+					/*
+					 * Aqui o usuário poderá modoficiar todos os dados do programa
+					 */
+					break;
+				case "5":
+					sair = false;
+			}
+		}
 	}
 }
