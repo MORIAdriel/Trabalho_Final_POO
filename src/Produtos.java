12bdee1732e61122e749fab17596b19d1506755d
Adriel Lenner Vinhal Mori <82418789+MORIAdriel@users.noreply.github.com>
2021-11-03 16:48:58 -0300

    Atualizção para adionar Produtos
    Adicionando toString, contrutores e alterado o objeto fornecedor

--- src/Produtos.java
+++ src/Produtos.java
@@ -5,18 +5,22 @@ package src;
  * @author Igor M. Padua e Adreil M. V. Mori
  */
 
-public class Produtos {
+public class Produtos{
 
-	String nome;
+	String nomeProduto;
 	private int quatidadeEstoque;
 	private double precoCusto;
 	double precoVenda;
 	private String dataUltimaCompraEstoque;
-	Fornecedor fornecedor;
+	Empresa fornecedora;
 
-	Produtos(String nome, int quatidadeEstoque) {
-		this.nome = nome;
+	Produtos(String nomeProduto, int quatidadeEstoque, double precoCusto, double precoVenda,
+			String dataUltimaCompraEstoque, Empresa fornecedora) {
+		this.nomeProduto = nomeProduto;
 		this.quatidadeEstoque = quatidadeEstoque;
+		this.precoCusto = precoCusto;
+		this.precoVenda = precoVenda;
+		this.dataUltimaCompraEstoque = dataUltimaCompraEstoque;
 	}
 
 	public void consulta(String nome) {
@@ -26,5 +30,13 @@ public class Produtos {
 	public void alteracaoCadastros() {
 
 	}
-
+	
+	public String toString() {
+		
+		return "Produto: Nome do produto: " + nomeProduto +
+			" - Quantidade em estoque: " + quatidadeEstoque +
+			" - Preço de custo: " + precoCusto +
+			" - Preço de venda: " + precoVenda +
+			" - Data da última ocorrência de compra: " + dataUltimaCompraEstoque +"\n";
+		}
 }
