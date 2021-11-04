252d2b08e23ab669cdc41f9b1a05a04cc0e711b5
igorpadua <igorpadua@tutanota.com>
2021-11-03 21:24:17 -0300

    Excluido o método de alteração

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -11,10 +11,6 @@ public class Fornecedor extends Empresa {
 		super(nome, telefone, cnpj, razaoSocial);	
 	}
 
-	public void alteracaoCadastros() {
-
-	}
-
 	public String toString() {
 		return "Fornecedor: " + super.toString() + " - " + estoque;
 	}
--- src/Loja.java
+++ src/Loja.java
@@ -1,7 +1,5 @@
 package src;
 
-import java.util.ArrayList;
-
 /*
  * Uma classe que representa uma loja  
  * @author Igor M. Padua e Adreil M. V. Mori
@@ -11,10 +9,6 @@ public class Loja extends Empresa {
 
 	Loja(String nome, String telefone, String cnpj, String razaoSocial) {
 		super(nome, telefone, cnpj, razaoSocial);	
-	}
-
-	public void alteracaoCadastros() {
-
 	}
 
 	public String toString() {
