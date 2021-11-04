f95cbbea92b21d610c9c26a66e170d7e8d2f56b3
igorpadua <igorpadua@tutanota.com>
2021-11-03 21:32:56 -0300

    Revert "Excluido o método de alteração"
    This reverts commit 252d2b08e23ab669cdc41f9b1a05a04cc0e711b5.

--- src/Fornecedor.java
+++ src/Fornecedor.java
@@ -11,6 +11,10 @@ public class Fornecedor extends Empresa {
 		super(nome, telefone, cnpj, razaoSocial);	
 	}
 
+	public void alteracaoCadastros() {
+
+	}
+
 	public String toString() {
 		return "Fornecedor: " + super.toString() + " - " + estoque;
 	}
